f81e2516865575b94aa2528b6bb940dc0a76ea45
Ricardo Vergutz <ricardovergutz@gmail.com>
2023-04-16 17:32:02 -0300

    feat: add sum function

---
+++ sum_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	result := sum(2,3)
+
+	if result!=5 {
+		t.Error("The result must be 5")
+	}
+}
+
+func TestSumX(t *testing.T) {
+	result := sumX(2,3)
+
+	if result!=7 {
+		t.Error("The result must be 7")
+	}
+}
+
+
+func TestTimes(t *testing.T) {
+	result := times(2,3)
+
+	if result!=6 {
+		t.Error("The result must be 6")
+	}
+}
+
+func TestSub(t *testing.T) {
+	result := sub(2,3)
+
+	if result!=-1 {
+		t.Error("The result must be -1")
+	}
+}
